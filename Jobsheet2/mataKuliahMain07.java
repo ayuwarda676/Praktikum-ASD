6ea27d067fc97b4af5b0c856da5469c8a628c2e5
ayuwarda676 <ayu020wardatul@gmail.com>
2026-02-20 11:03:47 +0700

    Tugas 1 Main

---
+++ Jobsheet2/mataKuliahMain07.java
@@ -0,0 +1,20 @@
+package Jobsheet2;
+
+public class mataKuliahMain07 {
+    public static void main(String[] args) {
+        mataKuliah07 mk1 = new mataKuliah07("SI211", "Pemrograman Algoritma dan Struktur Data", 3, 45);
+        mk1.tampilkanMatkul();
+
+        mataKuliah07 mk2 = new mataKuliah07("SI212", "Agama", 2, 30);
+        mk2.tampilkanMatkul();
+
+        mataKuliah07 mk3 = new mataKuliah07("SI213", "Praktikum Basis Data", 3, 45);
+        mk3.tampilkanMatkul();
+
+        mataKuliah07 mk4 = new mataKuliah07("SI214", "Sistem Operasi", 2, 45);
+        mk4.tampilkanMatkul();
+
+        mataKuliah07 mk5 = new mataKuliah07("SI215", "Aljabar Linier", 2, 30);
+        mk5.tampilkanMatkul();
+    }
+}

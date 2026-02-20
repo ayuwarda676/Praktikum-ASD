152c3186fa7b65ed660ee2f04fe853752263c067
ayuwarda676 <ayu020wardatul@gmail.com>
2026-02-20 11:14:56 +0700

    Tugas 1 Mata Kuliah Main

--- Jobsheet2/mataKuliahMain07.java
+++ Jobsheet2/mataKuliahMain07.java
@@ -2,19 +2,25 @@ package Jobsheet2;
 
 public class mataKuliahMain07 {
     public static void main(String[] args) {
-        mataKuliah07 mk1 = new mataKuliah07("SI211", "Pemrograman Algoritma dan Struktur Data", 3, 45);
+        mataKuliah07 mk1 = new mataKuliah07();
+        mk1.kodeMK = "SI211";
+        mk1.namaMK = "Pemrograman Berorientasi Objek";
+        mk1.sks = 3;
+        mk1.jumlahjam = 45;
         mk1.tampilkanMatkul();
 
-        mataKuliah07 mk2 = new mataKuliah07("SI212", "Agama", 2, 30);
+        mataKuliah07 mk2 = new mataKuliah07();
+        mk2.kodeMK = "SI213";
+        mk2.namaMK = "Praktikum Basis Data";
+        mk2.sks = 3;
+        mk2.jumlahjam = 45;
         mk2.tampilkanMatkul();
 
-        mataKuliah07 mk3 = new mataKuliah07("SI213", "Praktikum Basis Data", 3, 45);
-        mk3.tampilkanMatkul();
+        mk1.ubahSks(4);
+        mk1.tambahjam(2);
+        mk1.tampilkanMatkul();
 
-        mataKuliah07 mk4 = new mataKuliah07("SI214", "Sistem Operasi", 2, 45);
-        mk4.tampilkanMatkul();
-
-        mataKuliah07 mk5 = new mataKuliah07("SI215", "Aljabar Linier", 2, 30);
-        mk5.tampilkanMatkul();
+        mk2.kurangiJam(55);
+        mk2.tampilkanMatkul();
     }
 }

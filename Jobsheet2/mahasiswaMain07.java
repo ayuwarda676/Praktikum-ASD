112ac186d05b3a32cc4f0dfcc608970ce62b472c
ayuwarda676 <ayu020wardatul@gmail.com>
2026-02-20 10:52:53 +0700

    Percobaan 2 dan 3

---
+++ Jobsheet2/mahasiswaMain07.java
@@ -0,0 +1,21 @@
+package Jobsheet2;
+
+public class mahasiswaMain07 {
+    public static void main(String[] args) {
+        mahasiswa07 mhs1 = new mahasiswa07();
+        mhs1.nama = "Muhammad Ali farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI-2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI-2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+
+        mahasiswa07 mhs2 = new mahasiswa07("Annisa nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
+    }
+    
+}

b2bd91c0fd0f43026d486c31bbdac9bc09e087f2
ayuwarda676 <ayu020wardatul@gmail.com>
2026-02-20 10:50:09 +0700

    Percobaan 1 dan 2

--- Jobsheet2/mahasiswa07.java
+++ Jobsheet2/mahasiswa07.java
@@ -6,6 +6,17 @@ public class mahasiswa07 {
     String kelas;
     double ipk;
 
+    public mahasiswa07() {
+        
+    }
+
+    public mahasiswa07(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);

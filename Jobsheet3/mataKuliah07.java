abcf208ff700a538cdcf983fc421a5ac50fb1bd4
ayuwarda676 <ayu020wardatul@gmail.com>
2026-03-05 10:26:31 +0700

    Modifikasi Percobaan Mahasiswa 07

--- Jobsheet3/mataKuliah07.java
+++ Jobsheet3/mataKuliah07.java
@@ -6,11 +6,30 @@ public class mataKuliah07 {
     public int sks;
     public int jumlahJam;
 
+    public mataKuliah07() {
+    }
+
+
     public mataKuliah07(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
+
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode       : " + kode);
+        System.out.println("Nama       : " + nama);
+        System.out.println("SKS        : " + sks);
+        System.out.println("Jumlah Jam : " + jumlahJam);
+    }
+    
     
 }

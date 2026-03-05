645f60c2b28211b2dde0574db395e94c1b7263b9
ayuwarda676 <ayu020wardatul@gmail.com>
2026-03-05 10:03:25 +0700

    Percobaan Mahasiswa07

--- Jobsheet3/mataKuliah07.java
+++ Jobsheet3/mataKuliah07.java
@@ -1,5 +1,16 @@
 package Jobsheet3;
 
 public class mataKuliah07 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public mataKuliah07(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
     
 }

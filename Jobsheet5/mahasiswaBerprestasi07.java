d67418871d485ee55601ca7f7510bc3eaa842df6
ayuwarda676 <ayu020wardatul@gmail.com>
2026-04-10 16:35:10 +0700

    Modifikasi Insertion Sort Descending

--- Jobsheet5/mahasiswaBerprestasi07.java
+++ Jobsheet5/mahasiswaBerprestasi07.java
@@ -52,7 +52,7 @@ public class mahasiswaBerprestasi07 {
         for (int i = 1; i < idx; i++){
             mahasiswa07 temp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j-1].ipk > temp.ipk){
+            while (j > 0 && listMhs[j-1].ipk < temp.ipk){
                 listMhs[j] = listMhs[j-1];
                 j--;
             }

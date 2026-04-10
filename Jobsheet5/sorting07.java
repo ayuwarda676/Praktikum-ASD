c89f5c1ddef25c1cc07cf182cc3b689fb0c51e34
ayuwarda676 <ayu020wardatul@gmail.com>
2026-04-10 14:48:41 +0700

    Praktikum 1 Sorting

---
+++ Jobsheet5/sorting07.java
@@ -0,0 +1,35 @@
+package Jobsheet5;
+
+public class sorting07 {
+    int [] data;
+    int jumData;
+
+sorting07 (int Data[], int jmlDat){
+    jumData=jmlDat;
+    data= new int [jmlDat];
+    for (int i=0; i<jumData; i++){
+        data[i]=Data[i];
+    }
+}
+
+void bubbleSort(){
+    int temp=0;
+    for (int i=0; i<jumData-1; i++){
+        for (int j=1; j<jumData-1-i; j++){
+            if (data[j-1]>data[j]){
+                temp=data[j];
+                data[j]=data[j-1];
+                data[j-1]=temp;
+            }
+        }
+    }
+}
+
+void tampil(){
+    for (int i=0; i<jumData; i++){
+        System.out.print(data[i]+" ");
+    }
+    System.out.println();
+}
+
+}

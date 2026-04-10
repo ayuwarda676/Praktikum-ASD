b8e4d13b775870f08c22f5937588fe702115e942
ayuwarda676 <ayu020wardatul@gmail.com>
2026-04-10 15:06:29 +0700

    Percobaan 1 Insertion Sort

--- Jobsheet5/sortingMain07.java
+++ Jobsheet5/sortingMain07.java
@@ -4,8 +4,10 @@ public class sortingMain07 {
     public static void main(String[] args) {
     int a[] = {20, 10, 2, 7,12};
     int b[] = {30, 20, 2, 8, 14};
+    int c[] = {40, 10, 4, 9, 3};
     sorting07 dataurut1 = new sorting07(a, a.length);
     sorting07 dataurut2 = new sorting07(b, b.length);
+    sorting07 dataurut3 = new sorting07(c, c.length);
 
     System.out.println("Data awal 1");
     dataurut1.tampil();
@@ -19,6 +21,13 @@ public class sortingMain07 {
     dataurut2.selectionSort();
     System.out.println("Data setelah diurutkan dengan SELECTION SORT (ASC)");
     dataurut2.tampil();
-    
+
+    System.out.println();
+    System.out.println("Data awal 3");
+    dataurut3.tampil();
+    dataurut3.insertionSort();
+    System.out.println("Data setelah diurutkan dengan INSERTION SORT (ASC)");
+    dataurut3.tampil();
+
     }
 }

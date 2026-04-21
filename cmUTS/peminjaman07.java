19fa72d9e2b2283a258184feea8243241f56f7d2
ayuwarda676 <ayu020wardatul@gmail.com>
2026-04-21 10:06:06 +0700

    Modif CM

--- cmUTS/peminjaman07.java
+++ cmUTS/peminjaman07.java
@@ -8,11 +8,11 @@ public class peminjaman07 {
     int terlambat;
     int denda;
 
-    peminjaman07(mahasiswa07 mhs, buku07 buku, int lamaPinjam) {
+    peminjaman07(mahasiswa07 mhs, buku07 buku, int lamaPinjam, int denda) {
         this.mhs = mhs;
         this.buku = buku;
         this.lamaPinjam = lamaPinjam;
-        hitungDenda();
+        this.denda = denda;
     }
 
     void hitungDenda() {
@@ -25,13 +25,22 @@ public class peminjaman07 {
         }
     }
 
+    void statusKeterlambatan() {
+        if (lamaPinjam > 5) {
+            System.out.println("Status: Terlambat");
+        } else {
+            System.out.println("Status: Tepat Waktu");
+        }
+    }
+
     void tampilkanInfoPeminjaman() {
         System.out.println("Informasi Peminjaman: ");
         mhs.tampilkanInfo();
         buku.tampilkanInfo();
+        statusKeterlambatan();
         System.out.println("Lama Pinjam: " + lamaPinjam + " hari");
         System.out.println("Terlambat: " + terlambat + " hari");
         System.out.println("Denda: Rp " + denda);
     }
-    
+
 }

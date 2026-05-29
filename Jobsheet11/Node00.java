2d7f6d3460e2e019208e26fdd120afcfbc0a36ba
ayuwarda676 <ayu020wardatul@gmail.com>
2026-05-29 23:30:05 +0700

    Percobaan 1 Node

---
+++ Jobsheet11/Node00.java
@@ -0,0 +1,12 @@
+package Jobsheet11;
+
+public class Node00 {
+    mahasiswa00 data;
+    Node00 next;
+
+    public Node00(mahasiswa00 data, Node00 next) {
+        this.data = data;
+        this.next = next;
+    }
+    
+}

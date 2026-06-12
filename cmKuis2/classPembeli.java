99b9e8f073c079df4f88898aa738ef848844ebfc
ayuwarda676 <ayu020wardatul@gmail.com>
2026-06-12 09:26:59 +0700

    Kuis 2 class pembeli

---
+++ cmKuis2/classPembeli.java
@@ -0,0 +1,12 @@
+package cmKuis2;
+
+public class classPembeli {
+    String namaPembeli;
+    String noHp;
+
+    public classPembeli(String namaPembeli, String noHp) {
+        this.namaPembeli = namaPembeli;
+        this.noHp = noHp;
+    }
+    
+}
